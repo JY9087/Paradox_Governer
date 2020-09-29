776da2b75ceee644487abf635a452ae37fa9693f
JY9087 <1538378636@qq.com>
2020-09-29 14:36:38 +0800

    现在可以在任何一个Activity中使用数据库。 Record的PrimaryKey设置为autoGenerate。在构造函数中，uid使用0，

--- app/src/main/java/com/example/paradoxgoverner/CreateNewItem.kt
+++ app/src/main/java/com/example/paradoxgoverner/CreateNewItem.kt
@@ -3,30 +3,24 @@ package com.example.paradoxgoverner
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.EventLogTags
 import android.view.View
 import android.widget.EditText
 import androidx.annotation.ContentView
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomizeNewItem : AppCompatActivity() {
+class CreateNewItem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customization_of_new_item)
     }
 
-    fun CustomizeNewItem(view: View) {
+    fun CreateNewItem(view: View) {
         val editText = findViewById<EditText>(R.id.Description)
         val message = editText.text.toString()
 
-        //直接在数据库中添加
-        //uid不该有值的。但我不会autoGenerate，所以先用随机数
-        //这里insert没有成功
-        //AppDatabase.instance.userDAO().insertAll(Record((0 until 10000000).random(),message))
+        AppDatabase.instance.userDAO().insertAll(Record(0,message))
 
         val intent = Intent(this, MainActivity::class.java)
-            .putExtra(EXTRA_MESSAGE,message)
-            .putExtra(OPERATION_MESSAGE, OPERATION_DESCRIPTION)
         startActivity(intent)
     }
 }
