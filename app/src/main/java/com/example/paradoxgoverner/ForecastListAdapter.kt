51a9e458c8e914e0e8e10c554571aac09fc7bd03
JY9087 <1538378636@qq.com>
2020-09-30 12:14:03 +0800

    实现click事件

--- app/src/main/java/com/example/paradoxgoverner/ForecastListAdapter.kt
+++ app/src/main/java/com/example/paradoxgoverner/ForecastListAdapter.kt
@@ -1,11 +1,25 @@
 package com.example.paradoxgoverner
 
+import android.content.Context
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.properties.Delegates
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class ForecastListAdapter(var items: List<Record>):
     RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    companion object {
+        var instance:  ForecastListAdapter by Delegates.notNull()
+        fun instance() = instance
+    }
 
     //创建View
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -20,4 +34,5 @@ class ForecastListAdapter(var items: List<Record>):
     override fun getItemCount():  Int=items.size
 
     class ViewHolder(var textView:TextView):RecyclerView.ViewHolder(textView)
+
 }
