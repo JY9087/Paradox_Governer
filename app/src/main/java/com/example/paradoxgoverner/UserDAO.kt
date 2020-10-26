e77df917b8576ab22d3eb06f0a089305de6872f7
Super-tipi <71652178+Super-tipi@users.noreply.github.com>
2020-10-26 23:23:33 +0800

    Add files via upload

--- app/src/main/java/com/example/paradoxgoverner/UserDAO.kt
+++ app/src/main/java/com/example/paradoxgoverner/UserDAO.kt
@@ -1,48 +1,27 @@
 package com.example.paradoxgoverner
 
 import androidx.room.*
+import java.sql.Date
 
 @Dao
 interface UserDAO {
 
 
-    //查找Record
+    //查找
     @Query("SELECT * FROM Record WHERE Description LIKE :description " +
             "LIMIT 1")
-    fun findRecordByDescription(description: String): Record
-
-    @Query("SELECT * FROM Record WHERE uid LIKE :uid ")
-    fun findRecordByUid(uid : Int): Record
-
-    @Query("SELECT * FROM Record WHERE type LIKE :type ")
-    fun findRecordByType(type : String): List<Record>
-
-    @Query("SELECT * FROM Record WHERE member LIKE :member ")
-    fun findRecordByMember(member : String): List<Record>
-
-    @Query("SELECT * FROM Record WHERE category LIKE :category ")
-    fun findRecordByCategory(category : String): List<Record>
-
-    @Query("SELECT * FROM Record WHERE subcategory LIKE :subcategory")
-    fun findRecordBySubcategory(subcategory : String): List<Record>
-
-    @Query("SELECT * FROM Record WHERE merchant LIKE :merchant ")
-    fun findRecordByMerchant(merchant: String) : List<Record>
-
-    @Query("SELECT * FROM Record WHERE item LIKE :item ")
-    fun findRecordByItem(item: String) : List<Record>
-
-    @Query("SELECT * FROM Record WHERE account LIKE :account ")
-    fun findRecordByAccount(account: String) : List<Record>
-
-
-    //其他查找
+    fun findByName(description: String): Record
 
     //根据Category查找Subcategory
     @Query("SELECT * FROM Subcategory WHERE category LIKE :category")
     fun findSubcategoryByCategory(category : String): List<Subcategory>
 
 
+    @Query("SELECT * FROM Record WHERE uid LIKE :uid ")
+    fun findByUid(uid : Int): Record
+
+    @Query("SELECT * FROM Record WHERE type LIKE :type ")
+    fun findByType(type : String): List<Record>
 
     @Query("SELECT * FROM Member WHERE member LIKE :member ")
     fun findMemberByString(member : String): List<Member>
@@ -58,9 +37,6 @@ interface UserDAO {
 
     @Query("SELECT * FROM Item WHERE item LIKE :item ")
     fun findItemByString(item : String): List<Item>
-
-    @Query("SELECT * FROM Account WHERE account LIKE :account ")
-    fun findAccountByString(account : String): List<Account>
 
 
     //根据UID进行查找
@@ -79,15 +55,10 @@ interface UserDAO {
     @Query("SELECT * FROM Item WHERE uid LIKE :item ")
     fun findItemByUid(item : Int): List<Item>
 
-    @Query("SELECT * FROM Account WHERE uid LIKE :account ")
-    fun findAccountByUid(account : Int): List<Account>
-
-
-
 
     //全选
     @Query("SELECT * FROM Record")
-    fun getAllRecord(): List<Record>
+    fun getAll(): List<Record>
 
     @Query("SELECT * FROM Member")
     fun getAllMember(): List<Member>
@@ -103,9 +74,6 @@ interface UserDAO {
 
     @Query("SELECT * FROM Subcategory WHERE category LIKE :category")
     fun getAllSubcategory(category : String): List<Subcategory>
-
-    @Query("SELECT * FROM Account")
-    fun getAllAccount(): List<Account>
 
     @Query("SELECT * FROM Hidden")
     fun isInitialized(): List<Hidden>
@@ -133,9 +101,6 @@ interface UserDAO {
     fun insertAllItem(vararg items: Item)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAllAccount(vararg accounts : Account)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun initialize(vararg hide: Hidden)
 
 
@@ -144,7 +109,6 @@ interface UserDAO {
     //也许我会修改这部分代码
     @Delete
     fun delete(record: Record)
-
 
     @Delete
     fun deleteMember(member : Member)
@@ -161,18 +125,25 @@ interface UserDAO {
     @Delete
     fun deleteItem(item : Item)
 
-    @Delete
-    fun deleteAccount(account : Account)
 
-    //User Name And Password
-    @Insert
-    fun insertNameAndPwd(NameAndPwd:userNameAndPwd)
-    @Query("Select * From userNameAndPwd WHERE :element = userName")
-    fun searchNameAndPwd(element:String):Boolean
-    @Query("Select * From userNameAndPwd")
-    fun findAllUserNameAndPwd():List<userNameAndPwd>
-    @Query("Select password From userNameAndPwd WHERE :element = userName")
-    fun searchPwdByName(element: String):String
-    
+    //------------------------------------------------------------------------------
+
+    //按账户的流水查询，以下是降序和升序
+    @Query("SELECT * FROM Record WHERE account = (:SelectAccount)ORDER BY date,time DESC")
+    fun getWaterBillDesc(SelectAccount : String): List<Record>
+
+    @Query("SELECT * FROM Record WHERE account = (:SelectAccount)ORDER BY date,time ASC")
+    fun getWaterBillAsc(SelectAccount : String): List<Record>
+
+    @Query("SELECT * FROM Record WHERE account = (:SelectAccount)AND income=(:SelectIncome)")
+    fun getWaterBillIncome(SelectAccount : String,SelectIncome:Boolean):List<Record>
+
+    //给出日期就可以得到包括要求账户的每日流水数据包
+    @Query("SELECT * FROM Record WHERE account = (:SelectAccount) And date = (:SelectDate) AND income=(:SelectIncome) ORDER BY time DESC")
+    fun getDateWaterBill(SelectAccount : String,SelectDate : Date, SelectIncome:Boolean): List<Record>
+
+    //分类统计成员一级二级选项的数据包
+    @Query("SELECT * FROM Record WHERE account = (:SelectAccount) And date >=(:StartDate) and date<=(:EndDate) and member in (:SelectMember)AND category in(:SelectCategory)And subcategory IN (:SelectSubCategory) AND income=(:SelectIncome)")
+    fun getSubCategoryBill(SelectAccount : String, StartDate :Date,EndDate :Date, SelectMember : List<String>,SelectCategory:List<String> ,SelectSubCategory : List<String>, SelectIncome:Boolean): List<Record>
 
 }
